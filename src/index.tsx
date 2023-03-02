db47f1557fc1cd00e784a3fa8e09c5d9735ac0db
waldemarciupa <waldemarciupa90@gmail.com>
2023-03-02 20:02:24 +0100

    migrate App to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// @ts-ignore TODO: DELETE
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // @ts-ignore TODO: DELETE
